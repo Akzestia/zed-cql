fc5e2fa1066cc1afd76866c16efc8de692575f9a
アクゼスティア <akzestia@gmail.com>
2025-10-26 04:09:39 +0900

    Add outline.scm

---
+++ languages/cql/outline.scm
@@ -0,0 +1,10 @@
+; ((line_comment_with_outline
+;    (outline_identifier
+;      name: (_) @name)) )
+
+
+(line_comment_with_outline
+  (outline_identifier) @name) @item
+
+(block_comment
+  (outline_identifier) @name) @item
