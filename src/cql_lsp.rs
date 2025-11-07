b48a6976d3ce42d360797a1b0f6e5e025166a0bf
アクゼスティア <akzestia@gmail.com>
2025-11-08 03:22:29 +0900

    Update cql_lsp.rs

--- src/cql_lsp.rs
+++ src/cql_lsp.rs
@@ -1,6 +1,9 @@
 use serde::Deserialize;
 use zed::LanguageServerId;
-use zed_extension_api::{self as zed, Command, Result};
+use zed_extension_api::{
+    self as zed, Command, DebugAdapterBinary, DebugRequest, DebugScenario, DebugTaskDefinition,
+    Result, StartDebuggingRequestArgumentsRequest, TaskTemplate, Worktree, serde_json::Value,
+};
 
 #[derive(Debug, Deserialize)]
 struct CqlLspExtension {
@@ -48,6 +51,42 @@ impl zed::Extension for CqlLspExtension {
             args: vec![],
             env: Default::default(),
         })
+    }
+
+    fn get_dap_binary(
+        &mut self,
+        _adapter_name: String,
+        _config: DebugTaskDefinition,
+        _user_provided_debug_adapter_path: Option<String>,
+        _worktree: &Worktree,
+    ) -> Result<DebugAdapterBinary, String> {
+        todo!()
+    }
+
+    fn dap_request_kind(
+        &mut self,
+        _adapter_name: String,
+        _config: Value,
+    ) -> Result<StartDebuggingRequestArgumentsRequest, String> {
+        todo!()
+    }
+
+    fn dap_locator_create_scenario(
+        &mut self,
+        _locator_name: String,
+        _build_task: TaskTemplate,
+        _resolved_label: String,
+        _debug_adapter_name: String,
+    ) -> Option<DebugScenario> {
+        todo!()
+    }
+
+    fn run_dap_locator(
+        &mut self,
+        _locator_name: String,
+        _build_task: TaskTemplate,
+    ) -> Result<DebugRequest, String> {
+        todo!()
     }
 }
 
